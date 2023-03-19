5d278075dbdd58f8634d045053b7bcdec0bebf40
maxxxondagon <maxxondagon@gmail.com>
2023-03-19 19:35:11 +0500

    123

--- image/ViewController.swift
+++ image/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    let imageView = UIImageView(image: UIImage(named: "Image"))
+    let imageHeight: CGFloat = 270
+    let scrollView = UIScrollView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: imageHeight)
+        
+        scrollView.backgroundColor = .white
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.delegate = self
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.addSubview(imageView)
+        scrollView.frame = view.bounds
+        scrollView.contentSize = CGSize(
+            width: view.frame.width,
+            height: view.frame.height + imageHeight
+        )
+        view.addSubview(scrollView)
     }
-
-
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        print(offsetY)
+        scrollView.scrollIndicatorInsets = UIEdgeInsets(
+            top: imageView.frame.height - view.safeAreaInsets.top,
+            left: 0,
+            bottom: 0,
+            right: 0
+        )
+        if offsetY < 0 {
+            imageView.frame.origin.y = offsetY
+            imageView.frame.size.height = imageHeight - offsetY
+        }
+    }
 }
-
